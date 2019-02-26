422dc2d94668b526880ffc3263003d5283fe6d7e
Alenatim <alena_mail@ukr.net>
2019-02-26 19:02:55 +0200

    add

---
+++ components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+	<header>
+		<nav>
+			<ul>
+				<li><nuxt-link to="/">Starting page</nuxt-link></li>
+				<li><nuxt-link to="/recipes">Recipes</nuxt-link></li>
+				<li><nuxt-link to="/about">About</nuxt-link></li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<style scoped>
+header {
+	width: 100%;
+	height: 30px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: #e88cdd;
+}
+ul {
+	list-style: none;
+	padding: 0;
+	margin: 0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+li {
+	margin: 0 10px;
+}
+a {
+	text-decoration: none;
+}
+</style>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,54 @@
+<template>
+	<section class="recipes">
+		<Recipe 
+		v-for="recipe in recipes"
+		:key="recipe.id"
+		:thumbnail="recipe.thumbnail"
+		:title="recipe.title" 
+		:id="recipe.id"
+		:previewText="recipe.previewText" />
+	</section>
+</template>
+
+<script>
+import Recipe from '@/components/Recipe';
+export default{
+	components: {
+		Recipe
+	},
+	asyncData() {
+		return new Promise ((resolve, reject) => {
+			setTimeout(() => {
+				resolve({
+					recipes: [
+						{
+							id: "1",
+							title: "Delicious pizza",
+							previewText: "Awesome Pizza",
+							thumbnail: "https://www.patee.ru/r/x6/11/11/90/640m.jpg"
+						},
+						{
+							id: "2",
+							title: "Delicious salad",
+							previewText: "Very tasty indeed!",
+							thumbnail: "http://italianmeal.domestic-travel.ru/wp-content/uploads/sites/11/2016/10/salat_parmigiano.jpg"
+						}
+
+					]
+				})
+			},1500)
+		})
+	}
+};
+</script>
+
+<style scoped>
+.recipes {
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: center;
+	align-items: center;
+}
+
+
+</style>
